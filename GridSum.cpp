c2fa305f2a46b66b485a6b3bc33b8fb1ec10aa55
v1nnyc <cannallav019@yahoo.com>
2018-02-16 23:03:00 -0800

    gridsum is correct

--- GridSum.cpp
+++ GridSum.cpp
@@ -11,13 +11,50 @@
 #include "TwoD_Array.hpp"
 #include "GridSum.hpp"
 
+TwoD_Array<int>* graph = NULL;
+
 // Perform the precomputation step here
 GridSum::GridSum (TwoD_Array<int>& grid) {
+	for(int rows = 0; rows < grid.getNumRows(); rows++){
+		for(int cols = 0; cols < grid.getNumCols(); cols++ ){
+			if(cols != 0){
+				grid.at(rows, cols) = grid.at(rows, cols) + grid.at(rows, cols-1);
+			}
+		}
+	}
 
+	for(int cols = 0; cols < grid.getNumCols(); cols++ ){
+		for(int rows = 0; rows < grid.getNumRows(); rows++){
+			if(rows != 0){
+				grid.at(rows, cols) = grid.at(rows, cols) + grid.at(rows-1, cols);
+			}
+		}
+	}
+	graph = &grid;
 }
 
 // Perform the query step here
 int GridSum::query (int x1, int y1, int x2, int y2) {
+	graph->printOut();
+	std::cout<<graph->at(x1,y1) << "\n";
+	std::cout<<graph->at(x2,y2) << "\n";
+
+	int first = 0;
+	if(x1 != 0){
+		first = graph->at(x1-1,y2);
+	}
+	std::cout<<first << "\n";
+	int second = 0;
+	if(y1 != 0){
+		second = graph->at(x2,y1-1);
+	}
+std::cout<<second << "\n";
+	int third = 0;
+	if(y1 != 0 && x1 != 0){
+		third = graph->at(x1-1,y1-1);
+	}
+std::cout<<third << "\n";
+	return graph->at(x2,y2) - first - second + third;
   return 0;
 }
 
